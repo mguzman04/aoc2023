816f82eb1962adcfad8a1d48721ee2c9e668da59
Michael Guzman <mgzmn4@gmail.com>
2023-12-05 23:05:12 -0600

    Answer day01 part 1

--- src/day01/main.go
+++ src/day01/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"unicode"
 )
 
 func main() {
-	hi := "Hello!"
-	slice := []rune(hi)
-	fmt.Println(slice)
+	sum := 0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+		input := scanner.Text()
+		sum += getTwoDigitNumber(input)
+	}
+	fmt.Printf("Sum result: %v ", sum)
+
+	file.Close()
 }
 
 func getTwoDigitNumber(input string) int {
