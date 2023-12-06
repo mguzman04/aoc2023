29898163e4b419302e98f131def6beaf0c2a42b7
Michael Guzman <mgzmn4@gmail.com>
2023-12-06 15:05:46 -0600

    Passing day 1 part 2 tests

--- src/day01/day01_test.go
+++ src/day01/day01_test.go
@@ -62,3 +62,57 @@ func TestSum(t *testing.T) {
 		t.Errorf("Example failed. Expected 142, got %v", sum)
 	}
 }
+
+func TestRealDigits29(t *testing.T) {
+	twoNine := getRealDigits("two1nine")
+	if twoNine != 29 {
+		t.Errorf("getRealDigits(two1nine) = %v; want 29", twoNine)
+	}
+}
+func TestRealDigits83(t *testing.T) {
+	eightThree := getRealDigits("eightwothree")
+	if eightThree != 83 {
+		t.Errorf("getRealDigits(eightwothree) = %v; want 83", eightThree)
+	}
+}
+func TestRealDigits13(t *testing.T) {
+	oneThree := getRealDigits("abcone2threexyz")
+	if oneThree != 13 {
+		t.Errorf("getRealDigits(abcone2threexyz) = %v; want 13", oneThree)
+	}
+}
+func TestRealDigits24(t *testing.T) {
+	twoFour := getRealDigits("xtwone3four")
+	if twoFour != 24 {
+		t.Errorf("getRealDigits(xtwone3four) = %v; want 24", twoFour)
+	}
+}
+func TestRealDigits42(t *testing.T) {
+	fourTwo := getRealDigits("4nineeightseven2")
+	if fourTwo != 42 {
+		t.Errorf("getRealDigits(4nineeightseven2) = %v; want 42", fourTwo)
+	}
+}
+func TestRealDigits14(t *testing.T) {
+	oneFour := getRealDigits("zoneight234")
+	if oneFour != 14 {
+		t.Errorf("getRealDigits(zoneight234) = %v; want 14", oneFour)
+	}
+}
+func TestRealDigits76(t *testing.T) {
+	sevenSix := getRealDigits("7pqrstsixteen")
+	if sevenSix != 76 {
+		t.Errorf("getRealDigits(7pqrstsixteen) = %v; want 76", sevenSix)
+	}
+}
+
+func TestRealSum(t *testing.T) {
+	input := [7]string{"two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"}
+	sum := 0
+	for _, text := range input {
+		sum += getRealDigits(text)
+	}
+	if sum != 281 {
+		t.Errorf("Example failed. Expected 281, got %v", sum)
+	}
+}
--- src/day01/main.go
+++ src/day01/main.go
@@ -6,11 +6,37 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
+var numbers map[string]string
+
+func init() {
+	numbers = make(map[string]string)
+	// edge cases
+	numbers["oneight"] = "18"
+	numbers["threeight"] = "38"
+	numbers["fiveight"] = "58"
+	numbers["nineight"] = "98"
+	numbers["eightwo"] = "82"
+	numbers["sevenine"] = "79"
+	numbers["twone"] = "21"
+	// numbers
+	numbers["one"] = "1"
+	numbers["two"] = "2"
+	numbers["three"] = "3"
+	numbers["four"] = "4"
+	numbers["five"] = "5"
+	numbers["six"] = "6"
+	numbers["seven"] = "7"
+	numbers["eight"] = "8"
+	numbers["nine"] = "9"
+}
+
 func main() {
 	sum := 0
+	sum2 := 0
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -18,11 +44,16 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
 		input := scanner.Text()
+
+		// part 1
 		sum += getTwoDigitNumber(input)
+
+		// part 2
+		sum2 += getRealDigits(input)
 	}
-	fmt.Printf("Sum result: %v ", sum)
+	fmt.Printf("Part 1 result: %v\n ", sum)
+	fmt.Printf("Part 2 result: %v\n ", sum2)
 
 	file.Close()
 }
@@ -45,4 +76,16 @@ func getTwoDigitNumber(input string) int {
 
 func isNumber(char rune) bool {
 	return unicode.IsDigit((char))
+}
+
+func findAndReplace(input string) string {
+	newString := input
+	for key, value := range numbers {
+		newString = strings.ReplaceAll(newString, key, value)
+	}
+	return newString
+}
+
+func getRealDigits(input string) int {
+	return getTwoDigitNumber(findAndReplace((input)))
 }
